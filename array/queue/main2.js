f5baf9544a5fe57c3d0572c9d63fb632c929a994
anup4747 <anupdtarwade@gmail.com>
2025-09-18 21:14:58 +0530

    some changes

--- array/queue/main2.js
+++ array/queue/main2.js
@@ -1,3 +1,35 @@
-console.log("hello world")
-console.log("testing")
+class Queue{
+    constructor(size){
+        this.max = size; 
+        this.items = new Array(size);
+        this.currentSize = 0;
+        this.rear = -1;
+        this.front = -1;
+
+    }
+
+    enqueue(val){
+        if (this.currentSize <= this.max){
+            this.rear++;
+            this.items[this.rear] = val;
+            this.currentSize++
+        } else {
+            console.log("Queue is full")
+        }
+    }
+
+    dequeue(){
+        if (this.currentSize <= this.max && this.currentSize > -1 ){
+            this.rear--;
+            this.items.pop()
+            this.currentSize--;
+        } else {
+            console.log("Queue is empty already")
+        }
+    }
+}
+
+let queue = new Queue(5);
+
+
 
