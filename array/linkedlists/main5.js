5bf0995487f60fdee8203611aede65e6c03e2473
anup4747 <anupdtarwade@gmail.com>
2025-09-20 11:45:01 +0530

    inserting node in linked list

--- array/linkedlists/main5.js
+++ array/linkedlists/main5.js
@@ -1,4 +1,4 @@
-// searching Linked list node
+// insert node
 
 class List {
     constructor() {
@@ -7,10 +7,10 @@ class List {
             next: null,
         }
         this.tail = this.head;
-        this.size = 0 ;
+        this.size = 0;
     }
 
-    appendNode(nodeData){
+    appendNode(nodeData) {
         let newNode = {
             value: nodeData,
             next: null,
@@ -20,12 +20,12 @@ class List {
         this.size += 1
     }
 
-    traversing(){
+    traversing() {
         let counter = 0;
         let currentNode = this.head;
-        while (counter < this.size){
+        while (counter < this.size) {
             console.log(currentNode);
-            currentNode=currentNode.next;
+            currentNode = currentNode.next;
             counter++;
             // if (counter == 3){
             //     currentNode.value = 999;
@@ -33,13 +33,13 @@ class List {
         }
     }
 
-    deleteNode(pos){
+    deleteNode(pos) {
         let counter = 1;
         let lead = this.head;
-        if(pos === 1){
+        if (pos === 1) {
             this.head = this.head.next;
-        }else {
-            while(counter< pos-1){
+        } else {
+            while (counter < pos - 1) {
                 lead = lead.next;
                 counter++
             }
@@ -49,16 +49,34 @@ class List {
         }
     }
 
-    searchNode(pos){
+    searchNode(pos) {
         let counter = 0;
         let currentNode = this.head;
-        while (counter < this.size){
-            if (counter === pos-1){
+        while (counter < this.size) {
+            if (counter === pos-1) {
                 console.log(currentNode);
-            } 
-            currentNode=currentNode.next;
+            }
+            currentNode = currentNode.next;
             counter++;
         }
+    }
+
+    insertNode(index, value) {
+        let counter = 1;
+        let currentNode = this.head;
+        while (counter < index-1) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        let newNode = {
+            value: value,
+            next: currentNode.next,
+        };
+        currentNode.next = newNode;
+        if (index === this.size + 1) {
+            this.tail = newNode; 
+        }
+        this.size += 1;
     }
 }
 
@@ -70,6 +88,7 @@ node.appendNode(600)
 node.appendNode(700)
 node.appendNode(800)
 node.appendNode(900)
-// console.log(node)
-node.searchNode(3)
+node.insertNode(4,550)
+console.log(node)
+node.searchNode(4)
 // console.log(node.traversing())
